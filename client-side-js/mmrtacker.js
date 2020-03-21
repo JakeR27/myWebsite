d75f9669335543aab2ff1eafe5dec7f0dc844bb5
Jake <j.bailey-saltmarsh+contact@outlook.com>
2020-03-21 15:27:24 +0000

    fix

--- client-side-js/mmrtacker.js
+++ client-side-js/mmrtacker.js
@@ -13,14 +13,14 @@ function toggleView(collectionName) {
         if (collection[elem].type == "submit") {
             collection[elem].onclick = () => {
                 let theText = collection[elem -1].text
-                let ctx = collection[elem +1].getContext("2d")
+                let ctx = collection[2].getContext("2d")
                 ctx.font = "48px serif"
                 ctx.fillText("Hello World", 10,10)
             }
         }
         if (collection[elem].type == "reset") {
             collection[elem].onclick = () => {
-                let ctx = collection[elem -1].getContext("2d")
+                let ctx = collection[2].getContext("2d")
                 ctx.clearRect(0,0,collection[elem].width, collection[elem].height)
             }
         }
