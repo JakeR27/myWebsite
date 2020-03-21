6e7c615c40ece32028b7dc860c4026435e7dd234
Jake <j.bailey-saltmarsh+contact@outlook.com>
2020-03-21 15:03:57 +0000

    added logs

--- client-side-js/mmrtacker.js
+++ client-side-js/mmrtacker.js
@@ -1,9 +1,12 @@
 
 function toggleView(collectionName) {
 
-    let collection = document.getElementsByClassName(`${collectionName}`);
+    let collection = document.getElementsByClassName(collectionName);
+    console.log(collectionName)
+    console.log(collection)
 
     for (elem in collection) {
+        console.log(elem)
         elem.classList.toggle("state1");
         elem.classList.toggle("state2");
     }
