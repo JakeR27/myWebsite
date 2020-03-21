e5a8df87ea415487303f1630894dc6b5a33733cf
Jake <j.bailey-saltmarsh+contact@outlook.com>
2020-03-21 15:22:37 +0000

    added onclick events

--- client-side-js/mmrtacker.js
+++ client-side-js/mmrtacker.js
@@ -11,7 +11,18 @@ function toggleView(collectionName) {
         collection[elem].classList.toggle("state2");
 
         if (collection[elem].type == "submit") {
-console.log("FOUND A SUBMIT BUTTON")
+            collection[elem].onclick = () => {
+                let theText = collection[elem -1].text
+                let ctx = collection[elem +1].getContext("2d")
+                ctx.font = "48px serif"
+                ctx.fillText("Hello World", 10,10)
+            }
+        }
+        if (collection[elem].type == "reset") {
+            collection[elem].onclick = () => {
+                let ctx = collection[elem -1].getContext("2d")
+                ctx.clearRect(0,0,collection[elem].width, collection[elem].height)
+            }
         }
     }
 }
